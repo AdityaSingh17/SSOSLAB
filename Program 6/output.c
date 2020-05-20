4c9d21e61e3aa4ce98f2d435452ff120347ee508
AdityaSingh17 <adityarsingh17@gmail.com>
2020-05-20 15:18:34 +0530

    Added sample input and output C program

---
+++ Program 6/output.c
@@ -0,0 +1,10 @@
+
+
+#include<stdio.h>
+void main()     
+{
+    int a = 2, b = 3;
+    int sum = a+b;
+    int prodcut = a*b;
+    printf("\n Sum = %d \n Product = %d\n",sum,prodcut); 
+} 
---
+++ Program 6/sample.c
@@ -0,0 +1,11 @@
+//Author Aditya Rajneesh Singh
+/* C program to perform arithmetic */
+#include<stdio.h>
+void main()     // Main function
+{
+    int a = 2, b = 3;
+    int sum = a+b;
+    int prodcut = a*b;
+    printf("\n Sum = %d \n Product = %d\n",sum,prodcut); // Print result
+} 
+// End of program
